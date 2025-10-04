36178419ee2c66a9aa2fdfcbc91f06247221fc40
yejimandu <yeji3479@naver.com>
2025-10-04 14:32:04 +0900

    feat : 특정 사용자의 히스토리 정보 조회 개발

--- src/main/java/com/netpickz/core/user/repository/UserRatingInfoRepository.java
+++ src/main/java/com/netpickz/core/user/repository/UserRatingInfoRepository.java
@@ -1,10 +1,12 @@
 package com.netpickz.core.user.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.netpickz.core.user.entity.UserRatingInfoEntity;
 import com.netpickz.core.user.entity.pk.UserRatingInfoPK;
 
 public interface UserRatingInfoRepository extends JpaRepository<UserRatingInfoEntity, UserRatingInfoPK>{
-	
+	List<UserRatingInfoEntity> findByIdUserId(String userId);
 }
