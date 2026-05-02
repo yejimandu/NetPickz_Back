3f725fdea2d78e93f9a43195089ebaf57de2f239
yejiKimmmm <124652411+yejimandu@users.noreply.github.com>
2026-05-02 21:52:40 +0900

    Update SecurityConfig.java

--- src/main/java/com/netpickz/common/config/SecurityConfig.java
+++ src/main/java/com/netpickz/common/config/SecurityConfig.java
@@ -59,11 +59,13 @@ public class SecurityConfig {
 			.formLogin((auth) -> auth.disable())								// 폼 로그인 disable
 			.httpBasic((auth) -> auth.disable())								// http basic 인증 방식 disable
 			.authorizeHttpRequests((auth) -> auth								// 경로별 인가 작업
-				.requestMatchers(HttpMethod.POST, "/users", "/movies/search/multi").permitAll() 
-				.requestMatchers("/users/pw/reset", "/users/pw/reset/verify" ).authenticated()
-				.requestMatchers(HttpMethod.GET, "/users/**").permitAll() 
-                .requestMatchers("/auth/**", "/auth/login", "/auth/logout","/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                .requestMatchers("/movies/**", "/mail/pw/send").permitAll()  // TODO 추후에 패스 조절 필요
+				   .requestMatchers("/users/**").permitAll()
+								   // 원래 
+				//.requestMatchers(HttpMethod.POST, "/users", "/movies/search/multi").permitAll() 
+				//.requestMatchers("/users/pw/reset", "/users/pw/reset/verify" ).authenticated()
+				//.requestMatchers(HttpMethod.GET, "/users/**").permitAll() 
+                //.requestMatchers("/auth/**", "/auth/login", "/auth/logout","/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                //.requestMatchers("/movies/**", "/mail/pw/send").permitAll()  // TODO 추후에 패스 조절 필요
 //                .requestMatchers("/movies/**", "/mail/**").permitAll()  // TODO 추후에 패스 조절 필요
                 .anyRequest().authenticated())
 			.exceptionHandling((e) -> e.accessDeniedHandler(customAccessDeniedHandler).authenticationEntryPoint(jwtAuthenticationEntryPoint))
