c08e6f72ae9d2075731e31ffbcde48132ea4c73e
yejiKimmmm <124652411+yejimandu@users.noreply.github.com>
2026-05-11 17:06:16 +0900

    Update AuthServiceImpl.java

--- src/main/java/com/netpickz/core/auth/service/AuthServiceImpl.java
+++ src/main/java/com/netpickz/core/auth/service/AuthServiceImpl.java
@@ -65,7 +65,7 @@ public class AuthServiceImpl implements AuthService {
 	    var cookie = ResponseCookie
 	    		.from("refresh", refresh)
 	    		.httpOnly(true)
-	    		.secure(true)
+	    		.secure(false)    // HTTP 환경에서는 false
 	    		.path("/")
 //	            .sameSite("None") // CORS 환경에서 중요! 백엔드와 프론트 도메인 주소가 다를 때
 	            .sameSite("Strict")  // TODO 나중에 서버 올릴 떄 이거로 백엔드와 프론트 도메인 주소가 동일 할 때.
